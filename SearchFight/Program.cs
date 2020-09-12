c9f83e8f1dbdf95f000af73c4401f1ceca95cfb8
juanjosecs <juanjosechiroque@gmail.com>
2020-09-11 23:29:39 -0500

    upload project

---
+++ SearchFight/Program.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections.Generic;
+using SearchFight.Logic;
+using System;
+using System.Threading.Tasks;
+
+namespace SearchFight
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length > 0)
+            {
+                RunAsync(args).Wait();
+            }
+            else 
+            {
+                Console.WriteLine("Please, enter languages when call to exe file");
+                Console.WriteLine("");
+                Console.WriteLine("Loading initial languages");
+                string[] queries = new string[] { ".net", "java", "python", "javacript", "php" };
+                RunAsync(queries).Wait();                
+            }
+            
+            Console.ReadLine();
+        }
+
+
+        static async Task RunAsync(string[] queries)
+        {
+            SearchFightLogic searchFightLogic = new SearchFightLogic();
+            
+            long maxResultGoogle = 0, maxResultBing = 0;
+            int idxMajorGoogle = 0, idxMajorBing = 0;
+            long resultGoogle = 0, resultBing = 0;
+
+            List<string> resuts = new List<string>();
+
+            for (int i = 0; i < queries.Length; i++)
+            {
+                resultGoogle = await searchFightLogic.FindWithGoogle(queries[i]);
+                resultBing = await searchFightLogic.FindWithBing(queries[i]);
+                resuts.Add(string.Format("{0},{1},{2}", queries[i], resultGoogle, resultBing));
+
+                Console.WriteLine(string.Format("{0}: Google: {1} Bing: {2}", queries[i], resultGoogle, resultBing));
+
+                if (resultGoogle > maxResultGoogle) 
+                {
+                    maxResultGoogle = resultGoogle;
+                    idxMajorGoogle = i;
+                }
+
+                if (resultBing > maxResultBing)
+                {
+                    maxResultBing = resultBing;
+                    idxMajorBing = i;
+                }
+            }
+
+            Console.WriteLine("Google winner: " + queries[idxMajorGoogle]);
+            Console.WriteLine("Bing winner: " + queries[idxMajorBing]);
+            Console.WriteLine("Total winner: " + searchFightLogic.GetWinner(resuts));
+
+        }
+
+        
+    }
+}
---
+++ SearchFight/SearchFightLogic.cs
@@ -0,0 +1,99 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SearchFight.Logic
+{
+    public class SearchFightLogic
+    {
+        public SearchFightLogic() { }
+        
+        public async Task<long> FindWithGoogle(string query) 
+        {
+            long result = 0;
+            string api_key = ConfigurationManager.AppSettings["GOOGLE_API_KEY"];
+            string cx = ConfigurationManager.AppSettings["GOOGLE_CX"];
+
+            string urlBase = "https://www.googleapis.com/customsearch/v1?key={0}&cx={1}&q={2}";
+
+            string path = string.Format(urlBase, api_key, cx, query);
+
+            HttpClient client = new HttpClient();
+            HttpResponseMessage response = await client.GetAsync(path);
+
+            string responseStr;
+
+            if (response.IsSuccessStatusCode)
+            {
+                responseStr = await response.Content.ReadAsStringAsync();
+                var responseObj = JsonConvert.DeserializeObject<dynamic>(responseStr);
+                var googleResult = responseObj["queries"]["request"][0]["totalResults"];
+                result = googleResult;                
+            }
+
+            return result;
+
+        }
+        
+        public async Task<long> FindWithBing(string query)
+        {
+            long result = 0;
+            string api_key = ConfigurationManager.AppSettings["BING_API_KEY"];
+
+            string urlBase = "https://api.cognitive.microsoft.com/bing/v7.0/search?q={0}";
+
+            string path = string.Format(urlBase, query);
+
+            HttpClient client = new HttpClient();
+            client.DefaultRequestHeaders.Add("Ocp-Apim-Subscription-Key", api_key);
+            HttpResponseMessage response = await client.GetAsync(path);
+
+            string responseStr;
+
+            if (response.IsSuccessStatusCode)
+            {
+                responseStr = await response.Content.ReadAsStringAsync();
+                var responseObj = JsonConvert.DeserializeObject<dynamic>(responseStr);
+                var bingResult = responseObj["webPages"]["totalEstimatedMatches"];
+                result = bingResult;
+            }
+
+            return result;
+
+        }
+
+        public string GetWinner(List<string> summaryResults) 
+        {
+            string result = "";
+
+            long maxResult = 0;
+            long totalRow = 0;
+            
+            string[] rowItems;
+
+            foreach (var item in summaryResults)
+            {
+                rowItems = item.Split(',');
+                totalRow = Convert.ToInt64(rowItems[1]) + Convert.ToInt64(rowItems[2]);
+
+                if (totalRow > maxResult) 
+                {
+                    maxResult = totalRow;
+                    result = rowItems[0];
+                }
+
+            }
+
+            return result;
+        }
+
+    }
+
+
+}
